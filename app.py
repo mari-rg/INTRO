431f1f102f28a348b49da9427e61c27c7f4f7e0d
mari-rg <mar.jo11rg@gmail.com>
2026-08-20 13:26:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,10 @@ st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfac
 st.write("Facilmente puedo realizar backend y frontend.")
 image = Image.open('flor.jpg')
 st.image(image, caption='Interfaces multimodales')
+
+texto = st.text_input('Escribe algo', 'Este es mi texto')
+st.write('El texto escrito es', texto)
+
+st.subheader("Ahora usemos 2 Columnas")
+
+col1, col2 = st.columns(2)
