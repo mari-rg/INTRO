d8b7890268f33c99ebf1a73c0e01d4cc531e1244
mari-rg <mar.jo11rg@gmail.com>
2026-08-20 13:18:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
 st.title("HOLA! Mi Nombre es Majooo ")
+st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales.")
+st.write("Facilmente puedo realizar backend y frontend.")
+image = Image.open('https://github.com/mari-rg/INTRO/blob/main/flor.jpg ')
+st.image(image, caption='Interfaces multimodales')
