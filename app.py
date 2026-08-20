f7e257579c3a10626f06fb3ac3c1b0b04d06245d
mari-rg <mar.jo11rg@gmail.com>
2026-08-20 13:33:49 -0500

    Add two columns with interactive elements in app.py

--- app.py
+++ app.py
@@ -14,3 +14,13 @@ st.write('El texto escrito es', texto)
 st.subheader("Ahora usemos 2 Columnas")
 
 col1, col2 = st.columns(2)
+
+with col1:
+    st.subheader("Esta es la primera columna")
+    st.write("Las interfaces multimodales mejoran la experiencia de usuario")
+    resp = st.checkbox('Estoy de acuerdo')
+    if resp:
+        st.write('Correcto!')
+
+with col2:
+    st.subheader("Esta es la segunda columna")
